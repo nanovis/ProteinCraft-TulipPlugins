6f2ca85604aeaa4733b400eae322eb4473d52d0c
RodenLuo <roden.luo@gmail.com>
2025-03-23 18:07:23 +0300

    Update default node color in UMAPImport plugin from vivid blue to vivid red for improved visualization clarity.

--- import/UMAPImport.py
+++ import/UMAPImport.py
@@ -40,7 +40,7 @@ class UMAPImport(tlp.Algorithm):
         self.addColorParameter(
             "Node Color",
             "Color for the nodes in the visualization",
-            "(0, 128, 255, 255)",  # Vivid blue as default
+            "(0, 255, 95, 255)",  # Vivid red as default
             False,  # not mandatory
             True,   # input parameter
             False   # not output parameter
