cdcc8763fa937031b48338053c9d7097e41202ae
RodenLuo <roden.luo@gmail.com>
2025-04-06 22:23:40 +0300

    Update AF2igImport to modify CSV file path and expand selected properties
    - Changed the default CSV file path to 'outs_AF2ig_score.csv'.
    - Added multiple new selected properties for enhanced data handling.
    - Updated the selected properties order to include the new properties for improved state management.

--- import/AF2igImport.py
+++ import/AF2igImport.py
@@ -22,7 +22,7 @@ class AF2igImport(tlp.Algorithm):
             "CSV File",
             True,
             "Path to the CSV file to import",
-            "/home/luod/ProteinCraft/run/4_PD-L1/outs_AF2ig.csv"
+            "/home/luod/ProteinCraft/run/4_PD-L1/outs_AF2ig_score.csv"
         )
 
     def check(self):
@@ -90,11 +90,33 @@ class AF2igImport(tlp.Algorithm):
         selected_props["0"] = "binder_aligned_rmsd"
         selected_props["1"] = "pae_interaction" 
         selected_props["2"] = "plddt_total"
+        selected_props["3"] = "inter_chain_total"
+        selected_props["4"] = "inter_chain_without_vdw"
+        selected_props["5"] = "inter_chain_hbond"
+        selected_props["6"] = "inter_chain_vdw"
+        selected_props["7"] = "inter_chain_other"
+        selected_props["8"] = "binder_components_bonds"
+        selected_props["9"] = "binder_components_bonds_without_vdw"
+        selected_props["10"] = "binder_target_bonds"
+        selected_props["11"] = "binder_target_bonds_largest_component"
+        selected_props["12"] = "binder_target_bonds_no_vdw"
+        selected_props["13"] = "binder_target_bonds_no_vdw_largest_component"
 
         selected_props_order = {}
         selected_props_order["0"] = False
         selected_props_order["1"] = False
         selected_props_order["2"] = True
+        selected_props_order["3"] = True
+        selected_props_order["4"] = True
+        selected_props_order["5"] = True
+        selected_props_order["6"] = True
+        selected_props_order["7"] = True
+        selected_props_order["8"] = True
+        selected_props_order["9"] = True
+        selected_props_order["10"] = True
+        selected_props_order["11"] = True
+        selected_props_order["12"] = True
+        selected_props_order["13"] = True
         
         # Update the state
         state = pcv.state()
